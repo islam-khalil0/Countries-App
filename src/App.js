7d7709e5ed8732ad7c83617c9c358d4ac6d6d096
islam <eslamkhalil851@gmail.com>
2023-08-08 20:36:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+
+import { Routes, Route } from "react-router-dom";
+import CountryPage from "./Components/CountryPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/countries" element={<Home />} />
+        <Route path="countries/:countryCca2" element={<CountryPage />} />
+      </Routes>
     </div>
   );
 }
